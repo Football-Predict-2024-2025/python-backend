54067ed98456e3b98e60ef7183ac9291bd4c7f08
Dafa Aulia <122250992+Dapa19@users.noreply.github.com>
2024-08-31 18:47:11 +0700

    Add files via upload

---
+++ League_data/league.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, url_for
+
+app = Flask(__name__)
+
+@app.route('/api/league/get')
+def league_to_json():
+    # Data dengan URL gambar dari folder static
+    data = [
+        {
+            'league_id' : 1,
+            'league_name': 'English Premiere League',
+            'country': 'England',
+            'total_club': 20,
+            'league_icon': url_for('static', filename='images/EPL.png', _external=True)
+        },
+        {
+            'league_id' : 2,
+            'league_name': 'La Liga',
+            'country': 'Spain',
+            'total_club': 20,
+            'league_icon': url_for('static', filename='images/LaLiga.png', _external=True)
+        },
+        {
+            'league_id' : 3,
+            'league_name': 'Serie A',
+            'country': 'Italy',
+            'total_club': 20,
+            'league_icon': url_for('static', filename='images/SerieA.png', _external=True)
+        },
+        {
+            'league_id' : 4,
+            'league_name': 'League1',
+            'country': 'France',
+            'total_club': 18,
+            'league_icon': url_for('static', filename='images/League1.png', _external=True)
+        },
+        {
+            'league_id' : 5,
+            'league_name': 'Bundesliga',
+            'country': 'Germany',
+            'total_club': 18,
+            'league_icon': url_for('static', filename='images/Bundesliga.png', _external=True)
+        }
+    ]
+    # Mengembalikan data sebagai JSON
+    json_league =  jsonify(data)
+
+    return json_league
+
+if __name__ == '__main__':
+    app.run(debug=True)
